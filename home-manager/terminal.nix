7420e189bcba89ca90b23a7a28e4a958a81289e2
kylecs <kyle.spencer@gmail.com>
2025-11-15 12:47:16 -0500

    more testing

--- home-manager/terminal.nix
+++ home-manager/terminal.nix
@@ -1,11 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [
     ghostty
   ];
 
-  home.file = {
-    ".config".source = builtins.readFile ./dotfiles;
-    ".config".recursive = true;
+  home.file.".config/tmux" = {
+    source = ./dotfiles/tmux;
+    recursive = true;
   };
+
+  # home.file = {
+  #   ".config/tmux".source = builtins.readFile ./dotfiles/tmux;
+  #   ".config/tmux".recursive = true;
+  # };
 }
